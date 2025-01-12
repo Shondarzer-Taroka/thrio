19e35e6423706660b159582d9044d4029cd13f43
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-01-12 20:59:38 +0600

    orthographic camera

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ const cubeMaterials = new THREE.MeshBasicMaterial({ color: 'red' })
 
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterials)
 scene.add(cubeMesh)
-const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 200)
+// const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 200)
+const aspectRation=window.innerWidth/window.innerHeight
+const camera = new THREE.OrthographicCamera(-1*aspectRation,1*aspectRation,1,-1,0.1,200)
 camera.position.z = 5
 // scene.add(camera)
 
