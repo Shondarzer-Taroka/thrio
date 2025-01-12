8ead734037f1c701c462195e25fb00797b72370b
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-01-12 20:51:42 +0600

    perspective camera

--- src/main.js
+++ src/main.js
@@ -1,19 +1,30 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 const scene = new THREE.Scene()
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
 const cubeMaterials = new THREE.MeshBasicMaterial({ color: 'red' })
 
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterials)
 scene.add(cubeMesh)
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30)
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 200)
 camera.position.z = 5
 // scene.add(camera)
 
-const canvas=document.querySelector('.threejs')
+const canvas = document.querySelector('.threejs')
 console.log(canvas);
 
-const renderer=new THREE.WebGLRenderer({canvas:canvas})
-renderer.setSize(window.innerWidth,window.innerHeight)
-renderer.render(scene,camera)
-console.log(scene);
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping=true
+controls.autoRotate=true;
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+const renderLoop=()=>{
+  controls.update() 
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(renderLoop)
+}
+
+renderLoop()
 
